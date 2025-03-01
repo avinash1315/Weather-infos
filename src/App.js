cf182a3d3b843963bdd4f91394ede442e01b3423
Avinash mehto <avinash.mehto4@gmail.com>
2025-03-01 11:03:06 +0000

    ✨ Added weather components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./styles.css";
+import React from "react";
+import "./App.css";
+import Weather from "./components/Weather";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Weather />
     </div>
   );
 }
+
+export default App;
